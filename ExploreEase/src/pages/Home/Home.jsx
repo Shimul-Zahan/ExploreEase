449aaba9150f94f7c87e785ccac13d1aac529fb0
AlgoWebBot <shimulzahan636@gmail.com>
2023-12-19 02:52:17 +0600

    top destination part updated

--- ExploreEase/src/pages/Home/Home.jsx
+++ ExploreEase/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbabr from './Components/Navbabr'
 import Hero from './Components/Hero'
+import TopDestination from './Components/TopDestination'
 
 const Home = () => {
     return (
         <div>
             <Navbabr />
             <Hero />
+            <TopDestination />
         </div>
     )
 }
