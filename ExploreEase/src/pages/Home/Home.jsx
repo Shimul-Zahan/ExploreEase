2e0316420579c65519bee218f51b84efbdbdd999
AlgoWebBot <shimulzahan636@gmail.com>
2023-12-19 03:34:59 +0600

    explore the world part update

--- ExploreEase/src/pages/Home/Home.jsx
+++ ExploreEase/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbabr from './Components/Navbabr'
 import Hero from './Components/Hero'
 import TopDestination from './Components/TopDestination'
+import ExploreTheWorld from './Components/WxploreTheWorld'
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Navbabr />
             <Hero />
             <TopDestination />
+            <ExploreTheWorld />
         </div>
     )
 }
